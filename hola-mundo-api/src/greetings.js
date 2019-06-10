5a200e1949826535e9c842ac302d3aefe305b3c8
Martin Pirkl <martin.pirkl@strv.com>
2019-06-10 16:44:25 +0200

    Session 2 code fixes

--- hola-mundo-api/src/greetings.js
+++ hola-mundo-api/src/greetings.js
@@ -1,16 +1,9 @@
 /* eslint-disable no-process-env */
 'use strict'
 
-const { knex: db } = require('./database')
-
-const sendGreeting = async () => {
-  const myFoos = await db.select('*').from('foo')
-
-  return {
-    env: process.env.NODE_ENV,
-    message: 'Hola mundo!',
-    myFoos,
-  }
-}
+const sendGreeting = () => ({
+  env: process.env.NODE_ENV,
+  message: 'Hola mundo!!',
+})
 
 module.exports = sendGreeting
