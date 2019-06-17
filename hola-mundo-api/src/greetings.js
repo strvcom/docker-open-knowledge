914ed1f608727ef95575920e920923c1f5f8d20b
Martin Pirkl <martin.pirkl@strv.com>
2019-06-17 14:45:03 +0200

    Adjust hola mundo message to host-based

--- hola-mundo-api/src/greetings.js
+++ hola-mundo-api/src/greetings.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-process-env */
 'use strict'
 
+const os = require('os')
+
 const sendGreeting = () => ({
   env: process.env.NODE_ENV,
   message: 'Hola mundo!!',
+  hostname: `${os.hostname}`,
 })
 
 module.exports = sendGreeting
