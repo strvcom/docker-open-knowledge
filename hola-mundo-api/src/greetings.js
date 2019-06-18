c4ff80fbb8715a1f8280713d0888dae31a83949c
Martin Pirkl <martin.pirkl@strv.com>
2019-06-18 13:48:28 +0200

    Deployment test

--- hola-mundo-api/src/greetings.js
+++ hola-mundo-api/src/greetings.js
@@ -6,7 +6,7 @@ const os = require('os')
 const sendGreeting = () => ({
   env: process.env.NODE_ENV,
   message: 'Hola mundo!',
-  hostname: `${os.hostname}`,
+  hostname: `x${os.hostname}`,
 })
 
 module.exports = sendGreeting
