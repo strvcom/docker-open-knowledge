94e56f1fdb007222c01edfae953d0dd91edd922a
Martin Pirkl <martin.pirkl@strv.com>
2019-06-18 15:11:34 +0200

    Swtich to Heroku deployment

--- hola-mundo-api/.ci/deploy.sh
+++ hola-mundo-api/.ci/deploy.sh
@@ -1,6 +1,6 @@
 docker build -t $PACKAGE_LOCATION . --target prod
 
-# bash ./.ci/heroku-deploy.sh $HEROKU_APP_NAME $HEROKU_PROC_NAME $PACKAGE_LOCATION
-# bash ./.ci/heroku-release.sh $HEROKU_APP_NAME $HEROKU_PROC_NAME
+bash ./.ci/heroku-deploy.sh $HEROKU_APP_NAME $HEROKU_PROC_NAME $PACKAGE_LOCATION
+bash ./.ci/heroku-release.sh $HEROKU_APP_NAME $HEROKU_PROC_NAME
 
-bash ./.ci/ecs-deploy.sh $ECS_REPO $PACKAGE_LOCATION
+#bash ./.ci/ecs-deploy.sh $ECS_REPO $PACKAGE_LOCATION
